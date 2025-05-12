731ee270a9918ac91b171a133b075706377a8452
Lepros311 <andy.strickland@gmail.com>
2025-05-12 10:47:05 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Drinks;
+
+Console.Title = "Drinks";
+
+string? menuChoice;
+
+do
+{
+    menuChoice = UI.MainMenu();
+
+    switch (menuChoice)
+    {
+        case "-Close Application-":
+            Console.WriteLine("\nGoodbye!");
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+            break;
+        case "Cocktail":
+            //Display.PrintAllRecords("Ordinary Drink");
+            UI.ReturnToMainMenu();
+            break;
+        case "Punch/Party Drink":
+            //RecordsController.AddRecord();
+            UI.ReturnToMainMenu();
+            break;
+        case "Shake":
+            //RecordsController.EditRecord();
+            UI.ReturnToMainMenu();
+            break;
+        case "Other/Unknown":
+            //RecordsController.DeleteRecord();
+            UI.ReturnToMainMenu();
+            break;
+        case "Cocoa":
+            break;
+        case "Shot":
+            break;
+        case "Coffee/Tea":
+            break;
+        case "Homemade Liqueur":
+            break;
+        case "Beer":
+            break;
+        case "Soft Drink":
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+} while (menuChoice != "Close Application");
---
+++ UI.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+namespace Drinks
+{
+    internal class UI
+    {
+        public static string MainMenu()
+        {
+            Console.Clear();
+
+            var menuChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("SELECT A CATEGORY")
+                .PageSize(15)
+                .AddChoices(new[]
+                {
+                    "-Close Application-", "Cocktail", "Ordinary Drink", "Punch/Party Drink", "Shake", "Other/Unknown", "Cocoa", "Shot", "Coffee/Tea", "Homemade Liqueur", "Beer", "Soft Drink"
+                }));
+
+            return menuChoice;
+        }
+
+        public static void ReturnToMainMenu()
+        {
+            Console.Write("\nPress any key to return to the Main Menu...");
+            Console.ReadKey();
+        }
+    }
+}
+
